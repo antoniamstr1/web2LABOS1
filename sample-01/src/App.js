6251f05c29128d45d3dc97743cc6f392aee5ca3b
antoniamstr1 <mestrovic.antonia@gmail.com>
2023-10-23 18:43:08 +0200

    link in new tab fix 2

--- sample-01/src/App.js
+++ sample-01/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Router, Route, Switch } from "react-router-dom";
+import { Router, Route, Switch, HashRouter} from "react-router-dom";
 import { Container } from "reactstrap";
 
 import Loading from "./components/Loading";
@@ -34,7 +34,7 @@ const App = () => {
 
   return (
       <Auth0ProviderWithHistory>
-    <Router history={history}>
+    <HashRouter>
       <div id="app" className="d-flex flex-column h-100">
         <NavBar />
         <Container className="flex-grow-1 mt-5">
@@ -47,7 +47,7 @@ const App = () => {
         </Container>
         <Footer />
       </div>
-    </Router>
+    </HashRouter>
       </Auth0ProviderWithHistory>
   );
 };
