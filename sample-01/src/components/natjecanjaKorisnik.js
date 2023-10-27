8215b6c648cf88fb4e3f880328695e74dd138eb8
antoniamstr1 <mestrovic.antonia@gmail.com>
2023-10-27 20:11:27 +0200

    prikaz više 1:1 u kolu

--- sample-01/src/components/natjecanjaKorisnik.js
+++ sample-01/src/components/natjecanjaKorisnik.js
@@ -91,7 +91,7 @@ function PopisNatjecanja() {
                                     <div key={kolo.natjecatelj1 + kolo.natjecatelj2}>
                                         <div className="flex-container-vertical">
 
-                                            {index % Math.floor(br_nat(kola) / 2) === 0 && <div>{index2 + 1}. kolo:</div>}
+                                            {index % Math.floor(br_nat(kola) / 2) === 0 && <div>{index2++}. kolo:</div>}
 
                                             <div className="flex-container-horizontal div-smaller color1">
 
